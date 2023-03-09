7b96348ba89d6880fe1309a7ea5cbff4c017a7fb
cstepien <cyprian.stepien@sescom.eu>
2023-03-09 11:29:03 +0100

    Exception not utilized

--- src/CurrencyRates/RatesProvider/RatesProviderInterface.php
+++ src/CurrencyRates/RatesProvider/RatesProviderInterface.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\CurrencyRates\RatesProvider;
 
 use App\Collection\CollectionInterface;
+use App\Exception\FetchRatesAdapterException;
 
 interface RatesProviderInterface
 {
+    /**
+     * @throws FetchRatesAdapterException
+     */
     public function provide(string $baseCurrency) : CollectionInterface;
 }
